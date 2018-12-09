88f030693935e44724390b0eac39b000518afff4
Torsassi Naoures <torsassinaoures@gmail.com>
2018-12-09 22:27:06 +0100

    nhebek

--- app/src/main/java/com/example/admin/v1/CalculImc.java
+++ app/src/main/java/com/example/admin/v1/CalculImc.java
@@ -23,6 +23,7 @@ public class CalculImc extends AppCompatActivity {
     ImageView f4;
     DatabaseHelper helper;
     @Override
+    //n
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcul_imc);
